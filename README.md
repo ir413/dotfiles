390457df0378b1ebf04c267273735c1feb0c9637
Ilija Radosavovic <ilija.radosavovic@gmail.com>
2019-09-02 12:44:07 -0700

    [setup] Write a setup script

--- README.md
+++ README.md
@@ -2,8 +2,17 @@
 
 Repo for managing personal configuration files.
 
+## Installation
+
+Set up symlinks:
+
+```
+./setup.sh
+```
+
 ## Sources
 
 Dotfiles are derived from:
 
 - [Max Wang](https://github.com/mxw), [mxw/dotfiles](https://github.com/mxw/dotfiles)
+- [Carl Jackson](https://github.com/zenazn), [zenazn/dotfiles](https://github.com/zenazn/dotfiles)
---
+++ setup.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#
+# Sets up symlinks for dotfiles.
+
+# Asks a yes/no question
+function ask {
+  local question="$1"
+  read -p "$question? (y/N) "
+  local yn=$(echo "$REPLY" | tr "A-Z" "a-z")
+  test "$yn" == 'y' -o "$yn" == 'yes'
+}
+
+# Symlinks a file
+function symlink {
+  local from=$1
+  local from_p="$(pwd)/$from"
+  local to=$2
+  #local to_p="$HOME/$to"
+  local to_p="/tmp/$to"
+
+  if [ ! -e "$from_p" ]; then
+    printf "Error: $from_p does not exist\n"
+    return 1
+  fi
+
+  if [ ! -e "$to_p" ]; then
+    printf "Linking: ~/$to -> $from\n"
+    ln -s "$from_p" "$to_p"
+  else
+    local to_l=$(readlink "$to_p")
+    if [ "$?" == 0 -a \( "$to_l" == "$from_p" \) ]; then
+      printf "Link exists: ~/$to -> $from\n"
+    else
+      printf "File exists: $to\n"
+      if ask "Overwrite"; then
+        rm -f "$to_p"
+        symlink "$from" "$to"
+      fi
+    fi
+  fi
+}
+
+# Symlinks a file as dotfile
+function symlink_dot {
+  symlink "$1" ".$1"
+}
+
+# Dotfiles to symlink
+symlink_dot "bashrc"
+symlink_dot "inputrc"
+symlink_dot "vimrc"
+symlink_dot "gitconfig"
